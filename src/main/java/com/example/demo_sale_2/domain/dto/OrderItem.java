bcff0aa432568a9eaca10e48cc66587712d7b11e
Nguyen Vi <nguyenvi.cntt>
2022-05-14 08:17:33 +0700

    - add code

--- src/main/java/com/example/demo_sale_2/domain/dto/OrderItem.java
+++ src/main/java/com/example/demo_sale_2/domain/dto/OrderItem.java
@@ -1,5 +1,6 @@
 package com.example.demo_sale_2.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OrderItem {
 
     private String name;
